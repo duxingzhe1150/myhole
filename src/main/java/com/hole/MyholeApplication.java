85fe3f1f1f2404ad2bfc2919aa5920fc32ae6712
王超杰 <wangcj5@ziroom.com>
2018-04-25 11:56:01 +0800

    Initial commit

---
+++ src/main/java/com/hole/MyholeApplication.java
@@ -0,0 +1,12 @@
+package com.hole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyholeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyholeApplication.class, args);
+	}
+}
