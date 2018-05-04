dafecb66e84b849028d41a2410566894222b4c88
王超杰 <wangcj5@ziroom.com>
2018-05-04 11:08:44 +0800

    静态配置信息

---
+++ src/main/java/com/hole/config/Config.java
@@ -0,0 +1,19 @@
+package com.hole.config;
+
+/**
+ * @Author wangcj
+ * @date 2018/5/4 11:02
+ * @desc 存放静态配置信息
+ * @since 1.0
+ */
+public class Config {
+
+    public static class AppConfig {
+
+        public static final String APP_ID = "wxea30003bd18c17e7";
+
+        public static final String APP_SECRET = "649413a99959f419b6b2f0b808605d96";
+
+    }
+
+}
